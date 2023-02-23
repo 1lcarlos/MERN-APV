b23204642dc1a1fc66ecba7916ff4aeddf9598ac
lcarlos <luiscardozoicg@gmail.com>
2023-02-23 13:47:45 -0500

    create veterinario controller

---
+++ backend/controllers/veterinarioControllers.js
@@ -0,0 +1,14 @@
+const registrar = (req, res) => {
+  
+  const {nombre, email, password } = req.body;
+  console.log(nombre)
+  console.log(email)
+  console.log(password)
+
+  res.json({msg:"Registrando Usuario"});
+};
+
+const perfil = (req, res) => {
+    res.json({msg:"Mostrando perfil"});
+  }
+export { registrar, perfil };
