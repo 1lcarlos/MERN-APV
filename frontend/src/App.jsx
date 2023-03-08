0b0213343ec843f8dabfdb198d76c038ac0e2fee
lcarlos <luiscardozoicg@gmail.com>
2023-03-08 10:03:06 -0500

    Create pages publics and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
 import Login from "./layout/Login";
+import Registrar from "./layout/Registrar";
+import ConfirmarCuenta from "./layout/ConfirmarCuenta";
+import OlvidePassword from "./layout/OlvidePassword";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AuthLayout />}>
           <Route index element={<Login />} />
+          <Route path="registrar" element={<Registrar />} />
+          <Route path="olvide-password" element={<OlvidePassword />} />
+          <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
     </BrowserRouter>
