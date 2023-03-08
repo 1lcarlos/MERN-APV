1ee8a285e0c4a0aa231aaaa284494f30ab9bf8ff
lcarlos <luiscardozoicg@gmail.com>
2023-03-08 13:05:37 -0500

    Complete login page with navs links

--- frontend/src/layout/Login.jsx
+++ frontend/src/layout/Login.jsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom"
 const Login = () => {
   return (
     <>
@@ -41,6 +42,14 @@ const Login = () => {
             className=" bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase
             font-bold mt-5 hover:cursor-pointer hover:bg-indigo-900 md:w-auto " />
         </form>
+        <nav className="mt-10 lg:flex lg:justify-around">
+          <Link className="block text-center my-5 text-gray-500" to="/registrar">
+          ¿No tienes una cuenta? Registrate
+          </Link>
+          <Link className="block text-center my-5 text-gray-500" to="/olvide-password">
+          Olvide mi Password
+          </Link>
+        </nav>
       </div>
     </>
   );
