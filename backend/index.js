1864ee5fd44783cd611b48489f98ce2f3ca7f235
lcarlos <luiscardozoicg@gmail.com>
2023-03-10 13:41:19 -0500

    enable CORS front end

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
-import veterinarioRoutes from "./routes/veterinarioRoutes.js"
-import pacienteRoutes from "./routes/pacienteRoutes.js"
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+import pacienteRoutes from "./routes/pacienteRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -10,10 +11,23 @@ dotenv.config();
 
 conectarDB();
 
+const dominiosPermitidos = ["http://127.0.0.1:5173"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      //La peticion esta permitida
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`servidor en puerto ${PORT}`);
