af875f3d5d6588d37746165d7a2de71431dd6492
lcarlos <luiscardozoicg@gmail.com>
2023-03-11 10:12:29 -0500

    change url with client axios

--- frontend/src/layout/ConfirmarCuenta.jsx
+++ frontend/src/layout/ConfirmarCuenta.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
-import axios from "axios";
 import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
+
 const ConfirmarCuenta = () => {
   const [cuentaConfirmada, setCuentaConfirmada] = useState(false);
   const [cargando, setCargando] = useState(true);
@@ -13,8 +14,8 @@ const ConfirmarCuenta = () => {
   useEffect(() => {
     const confirmarCuenta = async () => {
       try {
-        const url = `http://localhost:4000/api/veterinarios/confirmar/${id}`;
-        const { data } = await axios(url);
+        const url = `/veterinarios/confirmar/${id}`;
+        const { data } = await clienteAxios(url);
         setCuentaConfirmada(true);
         setAlerta({
           msg: data.msg
