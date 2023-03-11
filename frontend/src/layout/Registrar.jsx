69758108b4a8800581a97d1258d7c38a94785078
lcarlos <luiscardozoicg@gmail.com>
2023-03-11 10:13:09 -0500

    change url for client axios

--- frontend/src/layout/Registrar.jsx
+++ frontend/src/layout/Registrar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
+import clienteAxios from "../config/axios";
 import Alerta from "../components/Alerta";
 const Registrar = () => {
   const [nombre, setNombre] = useState("");
@@ -28,8 +28,7 @@ const Registrar = () => {
     }
     setAlerta({});
     try {
-      const url = "http://localhost:4000/api/veterinarios/";
-      await axios.post(url, { nombre, email, password });
+      await clienteAxios.post("/veterinarios/", { nombre, email, password });
       setAlerta({
         msg: "Creado Correctamente, revisa tu email",
         error: false,
