bd36878c2d519d00ba9d024f7c2a4a8a2d507c80
lcarlos <luiscardozoicg@gmail.com>
2023-03-10 22:18:40 -0500

    create function emailRegistro

--- backend/controllers/veterinarioControllers.js
+++ backend/controllers/veterinarioControllers.js
@@ -1,6 +1,7 @@
 import Veterinario from "../models/Veterinario.js";
 import generarJWT from "../helpers/generarJWT.js";
 import generarId from "../helpers/generarId.js";
+import emailRegistro from "../helpers/emailRegistro.js";
 const registrar = async (req, res) => {
   const { nombre, email, password } = req.body;
 
@@ -14,6 +15,13 @@ const registrar = async (req, res) => {
   try {
     const veterinario = new Veterinario(req.body);
     const veterinarioGuardado = await veterinario.save();
+
+    //Enviar email
+    emailRegistro({
+      email,
+      nombre,
+      token: veterinarioGuardado.token,
+    });
 
     res.json(veterinarioGuardado);
   } catch (error) {
