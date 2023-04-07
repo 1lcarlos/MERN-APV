a2a5221963e11b16b6e309fcec8b4a7bfd64ad39
lcarlos <luiscardozoicg@gmail.com>
2023-04-07 10:52:57 -0500

    Add Auth provider for start useContext hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,21 @@ import Registrar from "./layout/Registrar";
 import ConfirmarCuenta from "./layout/ConfirmarCuenta";
 import OlvidePassword from "./layout/OlvidePassword";
 import NuevoPassword from "./layout/NuevoPassword";
+import { AuthProvider } from "./context/AuthProvider";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AuthLayout />}>
-          <Route index element={<Login />} />
-          <Route path="registrar" element={<Registrar />} />
-          <Route path="olvide-password" element={<OlvidePassword />} />
-          <Route path="olvide-password/:token" element={<NuevoPassword/>} />
-          <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route index element={<Login />} />
+            <Route path="registrar" element={<Registrar />} />
+            <Route path="olvide-password" element={<OlvidePassword />} />
+            <Route path="olvide-password/:token" element={<NuevoPassword />} />
+            <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
