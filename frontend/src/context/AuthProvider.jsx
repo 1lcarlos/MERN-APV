8dbe50d1be91d3c7a9ccb60e0ebc49c15434fe9c
lcarlos <luiscardozoicg@gmail.com>
2023-04-08 12:57:02 -0500

    Add value to use auth and setAuth

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -2,19 +2,21 @@ import { useState, useEffect, createContext, Children } from "react";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) =>{
-    
-    const [auth, setAuth] = useState({});
-    
-    return(
-        <AuthContext.Provider>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
 
-export{
-    AuthProvider
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
-export default AuthContext
+export { AuthProvider };
+
+export default AuthContext;
