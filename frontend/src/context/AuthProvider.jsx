357a6f6eb4d8c4e9802982aef9d50483a15e1acb
lcarlos <luiscardozoicg@gmail.com>
2023-04-18 17:32:04 -0500

    Finish authentication users

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -1,9 +1,31 @@
 import { useState, useEffect, createContext, Children } from "react";
+import clienteAxios from "../config/axios";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState({});
+
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+      const config = {
+        headers: {
+          "Content-Type": "aplication/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      try {
+        const { data } = await clienteAxios("veterinarios/perfil", config);
+        setAuth(data)
+      } catch (error) {
+        console.log(error.response.data.msg);
+        setAuth({})
+      }
+    };
+    autenticarUsuario();
+  }, []);
 
   return (
     <AuthContext.Provider
