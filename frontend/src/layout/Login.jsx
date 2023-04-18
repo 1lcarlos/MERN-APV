26b4b053ef207a57b4529de4f9a0cdd00a3c5cbe
lcarlos <luiscardozoicg@gmail.com>
2023-04-18 17:03:37 -0500

    Auth and generate token

--- frontend/src/layout/Login.jsx
+++ frontend/src/layout/Login.jsx
@@ -1,5 +1,41 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 const Login = () => {
+  const { auth, setAuth } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSumbit = async (e) => {
+    e.preventDefault();
+    
+     if ([email, password].includes("")) {
+      setAlerta({
+        msg: "Todos los campos son requeridos",
+        error: true,
+      });
+      return;
+    }
+    try {
+      const { data } = await clienteAxios.post("/veterinarios/login", {
+        email,
+        password,
+      });
+      localStorage.setItem('token', data.token)
+      
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    } 
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -9,7 +45,8 @@ const Login = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white ">
-        <form action="">
+        {msg && <Alerta alerta={alerta} />}
+        <form action="" onSubmit={handleSumbit}>
           <div className="my-5">
             <label
               className=" uppercase text-gray-600 block text-xl font-bold "
@@ -21,6 +58,8 @@ const Login = () => {
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl "
               type="email"
               placeholder="Email de registro"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -34,6 +73,8 @@ const Login = () => {
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl "
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <input
