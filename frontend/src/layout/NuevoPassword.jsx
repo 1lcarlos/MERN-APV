7ae7f8de9735bd164a926116b296afc152c1d11a
lcarlos <luiscardozoicg@gmail.com>
2023-06-25 14:19:01 -0500

    Change word pacientes for predios

--- frontend/src/layout/NuevoPassword.jsx
+++ frontend/src/layout/NuevoPassword.jsx
@@ -59,7 +59,7 @@ const NuevoPassword = () => {
       <div>
         <h1 className=" text-indigo-600 font-black text-6xl">
           Restablece tu password y no pierdas acceso a tus
-          <span className=" text-black"> Pacientes</span>
+          <span className=" text-black"> Predios</span>
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
