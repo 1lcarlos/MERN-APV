c91a2a6e44c909712a9b02dc93868dc51bb37372
lcarlos <luiscardozoicg@gmail.com>
2023-06-25 14:16:17 -0500

    Add routes protected, predios provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AuthLayout from "./layout/AuthLayout";
+import RutaProtegida from "./layout/RutaProtegida";
+
 import Login from "./layout/Login";
 import Registrar from "./layout/Registrar";
 import ConfirmarCuenta from "./layout/ConfirmarCuenta";
 import OlvidePassword from "./layout/OlvidePassword";
 import NuevoPassword from "./layout/NuevoPassword";
+import AdministradorPredios from "./paginas/AdministradorPredios";
 import { AuthProvider } from "./context/AuthProvider";
+import { PrediosProvider } from "./context/PrediosProvider";
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="registrar" element={<Registrar />} />
-            <Route path="olvide-password" element={<OlvidePassword />} />
-            <Route path="olvide-password/:token" element={<NuevoPassword />} />
-            <Route path="confirmar-cuenta/:id" element={<ConfirmarCuenta />} />
-          </Route>
-        </Routes>
+        <PrediosProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="registrar" element={<Registrar />} />
+              <Route path="olvide-password" element={<OlvidePassword />} />
+              <Route
+                path="olvide-password/:token"
+                element={<NuevoPassword />}
+              />
+              <Route
+                path="confirmar-cuenta/:id"
+                element={<ConfirmarCuenta />}
+              />
+            </Route>
+
+            <Route path="/admin" element={<RutaProtegida />}>
+              <Route index element={<AdministradorPredios />} />
+            </Route>
+          </Routes>
+        </PrediosProvider>
       </AuthProvider>
     </BrowserRouter>
   );
